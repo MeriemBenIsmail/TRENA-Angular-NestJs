556e3d1ae3ebc691d578ef71e1e242848b696045
meriem <meriembenismail@insat.u-carthage.tn>
2022-12-23 11:21:57 +0100

    added protected routes

--- src/terrains/terrains.controller.ts
+++ src/terrains/terrains.controller.ts
@@ -7,7 +7,9 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { CreateTerrainDto } from './dto/create-terrain.dto';
 import { UpdateTerrainDto } from './dto/update-terrain.dto';
 import { Terrain } from './schemas/terrain.schema';
@@ -17,16 +19,19 @@ import { TerrainsService } from './terrains.service';
 export class TerrainsController {
   constructor(private readonly terrainService: TerrainsService) {}
 
+  @UseGuards(JwtGuard)
   @Get(':terrainId')
   async getTerrain(@Param('terrainId') terrainId: string): Promise<Terrain> {
     return this.terrainService.getTerrainById(terrainId);
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   async getTerrains(): Promise<Terrain[]> {
     return this.terrainService.getTerrains();
   }
 
+  @UseGuards(JwtGuard)
   @Post()
   async createTerrain(
     @Body() createTerrainDTO: CreateTerrainDto,
@@ -34,6 +39,7 @@ export class TerrainsController {
     return this.terrainService.createTerrain(createTerrainDTO);
   }
 
+  @UseGuards(JwtGuard)
   @Patch(':terrainId')
   async updateTerrain(
     @Param('id') id: string,
@@ -41,6 +47,8 @@ export class TerrainsController {
   ): Promise<Terrain> {
     return this.terrainService.updateTerrain(id, updateTerrainDTO);
   }
+  
+  @UseGuards(JwtGuard)
   @Delete(':terrainId')
   async deleteTerrain(@Param('terrainId') terrainId: string): Promise<Terrain> {
     return this.terrainService.deleteTerrain(terrainId);
